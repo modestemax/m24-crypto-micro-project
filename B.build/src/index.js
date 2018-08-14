2672fa880ef528720c1679a59affd55a79f60fca
modestemax <modestemax@gmail.com>
2018-08-14 22:51:31 +0100

    ...

--- B.build/src/index.js
+++ B.build/src/index.js
@@ -9,11 +9,12 @@ const strategies = require('./strategies');
 redisSub.on('pmessage', async (pattern, channel, data) => {
     //debug(channel + ' received');
     if (/newData:.*/.test(channel)) {
-        debug('data received');
+        debug('data received', channel);
         const signal = JSON.parse(data);
-        for(let strategy in strategies){
+        for (let strategy in strategies) {
+            debug('checkin strategy', strategy);
             strategies[strategy].check(signal);
-        } 
+        }
     }
 });
 
