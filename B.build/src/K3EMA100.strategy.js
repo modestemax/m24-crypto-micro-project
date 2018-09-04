f80571d52b661da7c61169e92834a7836e999b2a
modestemax <modestemax@gmail.com>
2018-09-04 02:10:39 +0100

    sell

--- B.build/src/K3EMA100.strategy.js
+++ B.build/src/K3EMA100.strategy.js
@@ -10,15 +10,28 @@ module.exports = class extends Template {
         if (last && prev)
             if (prev.ema100 <= prev.bbb20)
                 if (last.ema100 > last.bbb20)
-                    if (last.macd < last.macdSignal) {
-                        let ticker = await this.getTicker({ exchange, symbolId });
-                        if (ticker && ticker.bid) {
-                            debug(`${symbolId} BID AT ${ticker.bid}`);
-                            return ticker.bid;
-                        }
-                        // this.pairFound({ side: 'BUY', symbolId, price: ticker.bid })
+                    if (last.macd < last.macdSignal)
+                        if (prev.ema10 <= prev.bbb20 && last.ema10 <= last.bbb20)
+                            if ((last.ema200 < last.ema100 && last.ema30 >= last.bbb20 && last.ema20 <= last.bbb20) || (last.ema200 > last.ema100 && last.ema50 < last.bbb20))
+                                if (last.close <= prev.bbb20) {
+                                    let ticker = await this.getTicker({ exchange, symbolId });
+                                    if (ticker && ticker.bid) {
+                                        debug(`${symbolId} BID AT ${ticker.bid}`);
+                                        return ticker.bid;
+                                    }
+                                    // this.pairFound({ side: 'BUY', symbolId, price: ticker.bid })
 
-                    }
+                                }
+    }
+    canSell({ exchange, symbolId, timeframe }, last, prev, signal) {
+        if (last && prev)
+            if ((prev.ema100 >= prev.bbb20 && last.ema100 < last.bbb20) || (prev.ema200 >= prev.bbb20 && last.ema200 < last.bbb20)) {
+                let ticker = await this.getTicker({ exchange, symbolId });
+                if (ticker && ticker.ask) {
+                    debug(`${symbolId} BID AT ${ticker.ask}`);
+                    return ticker.ask;
+                }
+            }
     }
 };
 
