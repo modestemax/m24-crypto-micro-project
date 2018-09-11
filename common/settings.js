f607a67217d625d54acb6a4f1417f5a6194e7aaf
modestemax <modestemax@gmail.com>
2018-09-12 00:33:38 +0100

    000

--- common/settings.js
+++ common/settings.js
@@ -39,7 +39,7 @@ module.exports = {
             timeInForce: 'GTC',
             isActive: true,
             doTrade: true,
-            takeProfit: 5,
+            takeProfit: 1,
         },
         "bbemaH1": {
             timeframe: 60,
