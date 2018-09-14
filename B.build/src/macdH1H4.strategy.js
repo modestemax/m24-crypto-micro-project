64bad5a077453db8547ab1613b453aa0cee0832b
modestemax <modestemax@gmail.com>
2018-09-15 00:50:32 +0100

    charly

--- B.build/src/macdH1H4.strategy.js
+++ B.build/src/macdH1H4.strategy.js
@@ -12,27 +12,27 @@ module.exports = class extends Template {
         //timeframe H4
         if (last && prev && lastH1 && prevH1 && currH1)
             if (last.macd > last.macdSignal)
-                if (prev.macd < prev.macdSignal)                    
-                        if (last.macdDistance > prev.macdDistance)
-                        
-                        if (last.ema10 > prev.ema10)  
+                if (prev.macd < prev.macdSignal)
+                    if (last.macdDistance > prev.macdDistance)
+
+                        if (last.ema10 > prev.ema10)
 
                             // if (candleH1.ema10 > candleH1.ema20)
-                                // if (lastH1.ema10 > lastH1.ema20)
-                                    if (prevH1.ema10 < lastH1.ema10)
-                                        if (lastH1.ema10 < currH1.ema10)
+                            // if (lastH1.ema10 > lastH1.ema20)
+                            if (prevH1.ema10 < lastH1.ema10)
+                                if (lastH1.ema10 < currH1.ema10)
 
-                                            if (lastH1.macdDistance > prevH1.macdDistance)
-                                                if (currH1.macdDistance > lastH1.macdDistance) {
-                                                    debug(`${symbolId} EMA H1 OK`);
-                                                    debug(`${symbolId} EMA H1 Trend OK`);
-                                                    let ticker = await this.getTicker({ symbolId });
-                                                    if (ticker && ticker.bid) {
-                                                        debug(`${symbolId} BID AT ${ticker.bid}`);
-                                                        return ticker.bid
-                                                        // return Math.min(ticker.bid, last.open);
-                                                    }
-                                                }
+                                    if (lastH1.macdDistance > prevH1.macdDistance)
+                                        if (currH1.macdDistance > lastH1.macdDistance) {
+                                            debug(`${symbolId} EMA H1 OK`);
+                                            debug(`${symbolId} EMA H1 Trend OK`);
+                                            let ticker = await this.getTicker({ symbolId });
+                                            if (ticker && ticker.bid) {
+                                                debug(`${symbolId} BID AT ${ticker.bid}`);
+                                                return ticker.bid
+                                                // return Math.min(ticker.bid, last.open);
+                                            }
+                                        }
     }
 };
 
