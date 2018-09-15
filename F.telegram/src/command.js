8faf5b74d6a4bb48f3799a5551df77dc89e0c935
modestemax <modestemax@gmail.com>
2018-09-15 17:01:30 +0100

    ..

--- F.telegram/src/command.js
+++ F.telegram/src/command.js
@@ -68,7 +68,7 @@ const commands = {
 		console.log('restart command received from ' + message.from.username)
 		tme.sendMessage({ chat_id: M24_LOG_CHAT_ID, text: 'restart command received from ' + message.from.username });
 		message.send("Restarting BOT");
-		// publish('m24:restart')
+		publish('m24:restart')
 
 	}
 }
