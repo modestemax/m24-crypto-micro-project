0f714a82e34db19fbc4493f53f8d8b8432b8487f
modestemax <modestemax@gmail.com>
2018-09-17 01:00:49 +0100

    noadx

--- B.build/src/K3SCALPING.strategy.js
+++ B.build/src/K3SCALPING.strategy.js
@@ -17,7 +17,7 @@ module.exports = class extends Template {
                     if ((current.ema20 >= current.bbb20) && (current.ema20 <= current.ema30)
                         || (current.ema20 < current.bbb20) && (current.ema20 > current.ema30) /*&& (current.ema20 >= current.ema10)*/)
                         if ((current.macd > current.macdSignal) /*|| (current.macd > 0)*/)
-                            if((currentH1.plusDI > currentH1.minusDI))
+                            // if((currentH1.plusDI > currentH1.minusDI))
                             if ((currentH1.macd > currentH1.macdSignal) || (currentH1.macdDistance >= lastH1.macdDistance)) {
                                 let ticker = await this.getTicker({ symbolId });
                                 if (ticker && ticker.ask) {
