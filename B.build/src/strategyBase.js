c930adf48759bdd73ce3c9fea175055059637abc
modestemax <modestemax@gmail.com>
2018-09-24 02:56:19 +0100

    s

--- B.build/src/strategyBase.js
+++ B.build/src/strategyBase.js
@@ -25,7 +25,7 @@ module.exports = class Strategy {
     async check(signal) {
         const { symbolId, timeframe, spread_percentage } = signal.candle;
         this.lastCheck = signal;
-        if (+timeframe === this.options.timeframe && spread_percentage < 1) {
+        if (+timeframe === this.options.timeframe && spread_percentage < .5) {
             this.StrategyLogThrottled(`I'm alive, checking ${this.lastCheck.candle.symbolId} now.`);
             this.subscribeOnce('m24:algo:check', (args) =>
                 this.StrategyLogThrottled(`I'm alive, checking ${this.lastCheck.candle.symbolId} now.`, args));
