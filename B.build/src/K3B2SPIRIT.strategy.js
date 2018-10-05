f739df598d5d63142339da4c00e793f69e617aa6
modestemax <modestemax@gmail.com>
2018-10-05 18:08:56 +0100

    ..

--- B.build/src/K3B2SPIRIT.strategy.js
+++ B.build/src/K3B2SPIRIT.strategy.js
@@ -9,21 +9,23 @@ module.exports = class extends Template {
 
     async canBuy({ symbolId, timeframe }, last, prev, signal) {
         const [currentX, lastX, prevX] = (await loadPoints({ symbolId, timeframe: this.options.timeframeX })).reverse();
+        const [candle, candle_1, candle_2, candle_3, candle_4] = (await loadPoints({ symbolId, timeframe: this.options.timeframe })).reverse();
         let current = signal.candle;
         if (last && prev && current && currentX && lastX) {
 
-            if ((current.ema20 >= current.bbb20) && (current.bbu20 / current.bbl20 >= 1.03) && (current.bbu20 / current.bbl20 <= 1.05))
-                if ((current.bbl20 < prev.bbl20) && (current.bbu20 > prev.bbu20))
-                    if (current.plus_di > current.minus_di)
-                        if ((current.ema10 > last.ema10) && (current.ema10 >= current.ema20))
-                            if ((current.macd_distance <= last.macd_distance) && (last.macd_distance > prev.macd_distance))
-                                if (current.close < current.bbu20) {
-                                    let ticker = await this.getTicker({ symbolId });
-                                    if (ticker && ticker.bid) {
-                                        console.log(`${symbolId} BID AT ${ticker.bid} ${ticker.now} `);
-                                        return ticker.bid;
+            if ((current.ema20 >= current.bbb20) && (current.bbu20 / current.bbl20 >= 1.01) && (current.bbu20 / current.bbl20 <= 1.05))
+                if ((last.bbl20 < prev.bbl20) && (prev.bbl20 < candle_3.bbl20))
+                    if ((last.bbu20 > prev.bbu20) && (prev.bbu20 > candle_3.bbu20))
+                        if ((last.plus_di >= 20) && (last.minus_di < 20) && (current.plus_di > 20))
+                            if ((current.ema10 >= last.ema10) && (current.ema10 >= current.ema20))
+                                if ((current.macd_distance >= last.macd_distance) && (last.macd_distance > prev.macd_distance))
+                                    if ((current.close <= current.bbu20) /*&& (currentX.close > currentX.open)*/) {
+                                        let ticker = await this.getTicker({ symbolId });
+                                        if (ticker && ticker.bid) {
+                                            console.log(`${symbolId} BID AT ${ticker.bid} ${ticker.now} `);
+                                            return ticker.bid;
+                                        }
                                     }
-                                }
         }
     }
 
@@ -32,13 +34,13 @@ module.exports = class extends Template {
         const [currentX, lastX, prevX] = (await loadPoints({ symbolId, timeframe: this.options.timeframeX })).reverse();
 
         // if (currentX && lastX)
-        if ((current.ema20 <= current.bbb20) && (last.ema20 > last.bbb20)) {
-            let ticker = await this.getTicker({ symbolId });
-            if (ticker && ticker.bid) {
-                debug(`${symbolId} ASK AT ${ticker.bid}`);
-                return ticker.bid;
-            }
-        }
+        // if ((current.ema20 <= current.bbb20) && (last.ema20 > last.bbb20)) {
+        //     let ticker = await this.getTicker({ symbolId });
+        //     if (ticker && ticker.bid) {
+        //         debug(`${symbolId} ASK AT ${ticker.bid}`);
+        //         return ticker.bid;
+        //     }
+        // }
     }
 
     getSellPriceIfSellable(rawAsset) {
