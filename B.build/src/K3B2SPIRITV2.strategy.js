0e493d59f8d0d71453f51b2b953e667e0f335cf7
modestemax <modestemax@gmail.com>
2018-10-11 17:02:02 +0100

    ...

--- B.build/src/K3B2SPIRITV2.strategy.js
+++ B.build/src/K3B2SPIRITV2.strategy.js
@@ -13,18 +13,19 @@ module.exports = class extends Template {
         let current = signal.candle;
         if (last && prev && current) {
 
-            if ((last.ema10 <= last.ema30) && (prev.ema10 <= prev.ema30) && (current.ema10 > current.ema30))
-                if ((current.ema10 > current.ema20) && (current.ema20 >= current.bbb20))
-                    if (current.bbu20 / current.bbl20 >= 1.013)
-                        if ((current.bbu20 / current.bbl20 > last.bbu20 / last.bbl20) && (last.bbu20 / last.bbl20 >= prev.bbu20 / prev.bbl20))
-                            if ((current.plus_di > 20) && (current.minus_di < 20))
-                                if (current.close <= current.bbu20) {
-                                    let ticker = await this.getTicker({ symbolId });
-                                    if (ticker && ticker.bid) {
-                                        console.log(`${symbolId} BID AT ${ticker.bid} ${ticker.now} `);
-                                        return ticker.bid;
+            if ((last.ema100 >= last.ema10) && (current.ema100 < current.ema10))
+                if ((current.ema20 >= current.ema30) && (current.ema10 > current.bbb20))
+                    if ((current.ema200 > current.ema100) && (current.ema50 <= current.ema20))
+                        if (current.bbu20 / current.bbl20 >= 1.013)
+                            if ((current.bbu20 / current.bbl20 > last.bbu20 / last.bbl20) && (last.bbu20 / last.bbl20 >= prev.bbu20 / prev.bbl20))
+                                if ((current.plus_di > 20) && (current.minus_di < 20) && (current.adx > 20))
+                                    if (current.close <= current.bbu20) {
+                                        let ticker = await this.getTicker({ symbolId });
+                                        if (ticker && ticker.bid) {
+                                            console.log(`${symbolId} BID AT ${ticker.bid} ${ticker.now} `);
+                                            return ticker.bid;
+                                        }
                                     }
-                                }
         }
     }
 
