5337ac72e881b72f94be41301f5ea42a2ae87d18
Modeste Nana <modestemax@gmail.com>
2018-10-19 01:48:35 +0100

    ...

--- B.build/src/m24day.strategy.js
+++ B.build/src/m24day.strategy.js
@@ -3,87 +3,86 @@ const debug = require('debug')('B:strategy:bbema150-15M');
 const M24Base = require('./m24Base');
 const _ = require('lodash');
 const { publish, subscribe, redisSet, redisGet } = require('common/redis');
-const { candleUtils, saveDatum, exchange, humanizeDuration } = require("common");
+const { candleUtils, saveDatum, exchange, humanizeDuration } = require('common');
 const { computeChange, valuePercent } = candleUtils;
 
 module.exports = class extends M24Base {
+	constructor(...args) {
+		super(...args);
+	}
+	setTracking({ id, open, symbolId, now, position, ...args }) {
+		this._stopTrackings = this._stopTrackings || {};
+		this._stopTrackings[id] = this._stopTrackings[id] || {};
+		this._stopTrackings[id][symbolId] = { open, now, position };
+	}
+	hasTracking({ id, open, symbolId, ...args }) {
+		return _.get(this._stopTrackings[id], symbolId);
+	}
+	async getTrackings(id) {
+		if (this._stopTrackings) {
+			return this._stopTrackings;
+		} else {
+			this._stopTrackings = (await redisGet(this.getTrackKey(id))) || { [id]: {} };
+		}
+	}
+	getTrackKey(id) {
+		return `m24trackings_${this.name}_${id}`;
+	}
+	async canBuy({ symbolId, timeframe }, last, prev, signal) {
+		let current = signal.candle;
+		await this.getTrackings(current.id);
+		if (current && last)
+			if (current.rating > 0)
+				if ((new Date(current.now) - new Date(current.time)) / (1e3 * 60) < this.options.timeframe / 2)
+					if (!this.hasTracking(current)) {
+						// const change = computeChange(current.open, current.close);
+						// const changeMax = computeChange(current.open, current.high);
 
-    constructor(...args) {
-        super(...args)
+						//if (change > this.options.enterThreshold && changeMax - change < 1)
+						// if (current.close > last.high)
+						// if (current.close > (_.max([last.open, last.close]) + last.high) / 2)
+						if (current.close > (last.close + last.high) / 2)
+							if (current.position <= 5)
+								if (current.change_from_open > this.options.change_from_open_min) {
+									this.setTracking(current);
+									redisSet({
+										key: this.getTrackKey(current.id),
+										data: await this.getTrackings(current.id),
+										expire: 60 * 60 * 24 * 7
+									});
+									return true;
+								}
+					}
+	}
 
-    }
-    setTracking({ id, open, symbolId, now, position, ...args }) {
-        this._stopTrackings = this._stopTrackings || {};
-        this._stopTrackings[id] = this._stopTrackings[id] || {};
-        this._stopTrackings[id][symbolId] = { open, now, position }
-    }
-    hasTracking({ id, open, symbolId, ...args }) {
-        return _.get(this._stopTrackings[id], symbolId)
-    }
-    async   getTrackings(id) {
-        if (this._stopTrackings) {
-            return this._stopTrackings;
-        } else {
-            this._stopTrackings = await redisGet(this.getTrackKey(id)) || { [id]: {} }
-        }
-    }
-    getTrackKey(id) {
-        return `m24trackings_${this.name}_${id}`;
-    }
-    async canBuy({ symbolId, timeframe }, last, prev, signal) {
-        let current = signal.candle;
-        await this.getTrackings(current.id);
-        if (current && last)
-            if (current.rating > 0)
-                if ((new Date(current.now) - new Date(current.time)) / (1e3 * 60) < this.options.timeframe / 2)
-                    if (!this.hasTracking(current)) {
-                        // const change = computeChange(current.open, current.close);
-                        // const changeMax = computeChange(current.open, current.high);
+	async canSell({ symbolId, timeframe }, last, prev, signal) {}
 
-                        //if (change > this.options.enterThreshold && changeMax - change < 1)
-                        // if (current.close > last.high)
-                        // if (current.close > (_.max([last.open, last.close]) + last.high) / 2)
-                        if (current.close > (last.close + last.high) / 2)
-                            if (current.position <=5)
-                                if (current.change_from_open > this.options.change_from_open_min) {
-                                    this.setTracking(current);
-                                    redisSet({
-                                        key: this.getTrackKey(current.id),
-                                        data: await this.getTrackings(current.id),
-                                        expire: 60 * 60 * 24 * 7
-                                    });
-                                    return true;
-
-                                }
-                    }
-    }
-
-    async canSell({ symbolId, timeframe }, last, prev, signal) {
-
-
-
-    }
-
-    getSellPriceIfSellable(rawAsset) {
-        const { change, maxChange, minChange, openPrice, closePrice, symbolId, timestamp } = rawAsset;
-        const H1 = 1e3 * 60 * 60;
-        const M1 = 1e3 * 60;
-        const price = this.prices[symbolId];
-        const duration = Date.now() - timestamp;
-
-        // if ((maxChange - change) / maxChange > .5) {
-        //     return true
-        // }
-        if ((maxChange - change) > 3) {
-            return true
-        }
-        if (change < maxChange && minChange < 2 && change > .5 && change < 1) {
-            return true
-        }
-        if (change < this.options.stopLoss || (change <= -2 && maxChange <= 0)) {
-            return true;
-        }
-        return valuePercent(openPrice, this.options.takeProfit);
-    }
+	getSellPriceIfSellable(rawAsset) {
+		const { change, maxChange, minChange, openPrice, closePrice, symbolId, timestamp } = rawAsset;
+		const H1 = 1e3 * 60 * 60;
+		const M1 = 1e3 * 60;
+		const price = this.prices[symbolId];
+		const duration = Date.now() - timestamp;
+		const current = _.get(this.signal, `[${symbolId}].candle`);
+		// if ((maxChange - change) / maxChange > .5) {
+		//     return true
+		// }
+		if (current && current.position > 5) {
+			if (change > 0.5) {
+				return valuePercent(openPrice, change);
+			} else {
+				return valuePercent(openPrice, 0.5);
+			}
+		}
+		if (maxChange - change > 3) {
+			return true;
+		}
+		if (change < maxChange && minChange < 2 && change > 0.5 && change < 1) {
+			return true;
+		}
+		if (change < this.options.stopLoss || (change <= -2 && maxChange <= 0)) {
+			return true;
+		}
+		return valuePercent(openPrice, this.options.takeProfit);
+	}
 };
-
--- B.build/src/strategyBase.js
+++ B.build/src/strategyBase.js
@@ -1,7 +1,7 @@
 const debug = require('debug')('B:strategy-base');
 const _ = require('lodash');
 
-const [SELL, BUY] = ['SELL', 'BUY'];
+const [ SELL, BUY ] = [ 'SELL', 'BUY' ];
 
 const { publish, subscribe } = require('common/redis');
 const { tradingView, candleUtils, computeChange, exchange, market, fetchTickers, fetchBalance } = require('common');
@@ -9,148 +9,155 @@ const { getAssetBalance } = market;
 const { findSignal, loadPoints } = candleUtils;
 
 module.exports = class Strategy {
+	constructor({ name, ...options }) {
+		Object.assign(this, { bid: null, name, options, tickers: {}, prices: {}, signal: {} });
+		publish('m24:algo:loaded', `#${name} loaded`);
+		this.StrategyLogThrottled = _.throttle(this.StrategyLog.bind(this), 1e3 * 60 * 5);
+		this.subscribeOnce = _.once(subscribe);
+		fetchTickers(this.onFetchTickers.bind(this));
+	}
+	onFetchTickers(price, assets) {
+		Object.assign(this.tickers, assets);
+		this.prices[price.info.symbol] = price;
+		this.tick(price);
+	}
+	tick(price) {}
+	async check(signal) {
+		const { symbolId, timeframe, spread_percentage } = signal.candle;
+		// const [current24] = (await loadPoints({ symbolId, timeframe: 60 * 24 })).reverse();
 
-    constructor({ name, ...options }) {
-        Object.assign(this, { bid: null, name, options, tickers: {}, prices: {}, signal: {} });
-        publish('m24:algo:loaded', `#${name} loaded`);
-        this.StrategyLogThrottled = _.throttle(this.StrategyLog.bind(this), 1e3 * 60 * 5);
-        this.subscribeOnce = _.once(subscribe)
-        fetchTickers(this.onFetchTickers.bind(this));
-    }
-    onFetchTickers(price, assets) {
-        Object.assign(this.tickers, assets)
-        this.prices[price.info.symbol] = price;
-        this.tick(price);
-    }
-    tick(price) { }
-    async check(signal) {
-        const { symbolId, timeframe, spread_percentage } = signal.candle;
-        // const [current24] = (await loadPoints({ symbolId, timeframe: 60 * 24 })).reverse();
+		this.lastCheck = signal;
+		// const change24 = computeChange(current24.open, current24.close);
+		// const change24Max = computeChange(current24.open, current24.high);
 
-        this.lastCheck = signal;
-        // const change24 = computeChange(current24.open, current24.close);
-        // const change24Max = computeChange(current24.open, current24.high);
+		// if (change24 > 2)
+		if (+timeframe === this.options.timeframe && spread_percentage < 1) {
+			this.StrategyLogThrottled(`I'm alive, checking ${this.lastCheck.candle.symbolId} now.`);
+			this.subscribeOnce('m24:algo:check', (args) =>
+				this.StrategyLogThrottled(`I'm alive, checking ${this.lastCheck.candle.symbolId} now.`, args)
+			);
+			const last = signal.candle_1;
+			const prev = signal.candle_2;
+			const market = exchange.marketsById[symbolId];
+			if (market) {
+				let canBuy = await this.canBuy(signal.candle, last, prev, signal, this.tickers[market.symbol]);
+				let canSell = await this.canSell(signal.candle, last, prev, signal, this.tickers[market.symbol]);
 
-        // if (change24 > 2)
-        if (+timeframe === this.options.timeframe && spread_percentage < 1) {
-            this.StrategyLogThrottled(`I'm alive, checking ${this.lastCheck.candle.symbolId} now.`);
-            this.subscribeOnce('m24:algo:check', (args) =>
-                this.StrategyLogThrottled(`I'm alive, checking ${this.lastCheck.candle.symbolId} now.`, args));
-            const last = signal.candle_1;
-            const prev = signal.candle_2;
-            const market = exchange.marketsById[symbolId];
-            if (market) {
-                let canBuy = await this.canBuy(signal.candle, last, prev, signal, this.tickers[market.symbol]);
-                let canSell = await this.canSell(signal.candle, last, prev, signal, this.tickers[market.symbol]);
+				if (canBuy || canSell) {
+					let ticker = await this.getTicker({ symbolId });
+					let bid = _.get(ticker, this.options.buyMode === 'limit' ? 'bid' : 'ask');
+					let ask = _.get(ticker, this.options.sellMode === 'limit' ? 'ask' : 'bid');
+					let now = _.get(ticker, 'now');
+					bid && console.log(`${this.name} ${symbolId} BID AT ${bid} ${now} `);
+					ask && console.log(`${this.name} ${symbolId} ASK AT ${ask} ${now} `);
 
-                if (canBuy || canSell) {
-                    let ticker = await this.getTicker({ symbolId });
-                    let bid = _.get(ticker, this.options.buyMode === 'limit' ? 'bid' : 'ask')
-                    let ask = _.get(ticker, this.options.sellMode === 'limit' ? 'ask' : 'bid')
-                    let now = _.get(ticker, 'now')
-                    bid && console.log(`${this.name} ${symbolId} BID AT ${bid} ${now} `);
-                    ask && console.log(`${this.name} ${symbolId} ASK AT ${ask} ${now} `);
+					Object.assign(this, { symbolId, bid, ask, timeframe });
+					if (bid) {
+						debug(`${this.name} Buy OK`);
+						this.notifyBuy();
+					} else if (ask) {
+						debug(`${this.name} Sell OK`);
+						this.notifySell();
+					}
+				}
+			}
+		}
+	}
+	selfSell(asset) {
+		let ask = this.getSellPriceIfSellable(asset);
+		if (ask) {
+			if (typeof ask === 'boolean') {
+				delete asset.closePrice;
+				publish('crypto:sell_market', asset);
+			} else {
+				asset.closePrice = ask;
+				publish('crypto:sell_limit', asset);
+			}
+		}
+	}
 
-                    Object.assign(this, { symbolId, bid, ask, timeframe });
-                    if (bid) {
-                        debug(`${this.name} Buy OK`);
-                        this.notifyBuy();
-                    } else if (ask) {
-                        debug(`${this.name} Sell OK`);
-                        this.notifySell();
-                    }
-                }
-            }
-        }
-    }
-    selfSell(asset) {
-        let ask = this.getSellPriceIfSellable(asset)
-        if (ask) {
-            if (typeof ask === 'boolean') {
-                delete asset.closePrice;
-                publish('crypto:sell_market', asset)
-            } else {
-                asset.closePrice = ask
-                publish('crypto:sell_limit', asset)
-            }
-        }
-    }
+	getSellPriceIfSellable(asset) {}
+	canBuy({ symbolId, timeframe }, last, prev, signal) {}
+	canSell({ symbolId, timeframe }, last, prev, signal) {}
+	async notifyBuy() {
+		const market = exchange.marketsById[this.symbolId];
+		const balance = getAssetBalance(market.base);
+		const balanceBTC = getAssetBalance(market.quote, 'free');
 
-    getSellPriceIfSellable(asset) {
+		const { name: strategyName, options, bid, symbolId, timeframe } = this;
+		this.pairFound({ side: BUY, symbolId, price: bid, test: !options.doTrade });
 
-    }
-    canBuy({ symbolId, timeframe }, last, prev, signal) { }
-    canSell({ symbolId, timeframe }, last, prev, signal) { }
-    async notifyBuy() {
-        const market = exchange.marketsById[this.symbolId];
-        const balance = getAssetBalance(market.base);
-        const balanceBTC = getAssetBalance(market.quote, 'free');
+		if (!balance && balanceBTC > market.limits.cost.min) {
+			this.notify(BUY);
+		} else {
+			this.StrategyLog(`Buy event not published, balance insufisante #${symbolId} #no_balance_${symbolId}`);
+		}
+	}
+	async notifySell() {
+		const market = exchange.marketsById[this.symbolId];
+		const balance = getAssetBalance(market.base, 'free');
+		if (balance) {
+			this.notify(SELL);
+		}
+	}
+	notify(side) {
+		const { name: strategyName, options, ask: closePrice, bid: openPrice, symbolId, timeframe } = this;
+		let order = { strategyName, openPrice, closePrice, symbolId, timeframe };
 
-        const { name: strategyName, options, bid, symbolId, timeframe } = this;
-        this.pairFound({ side: BUY, symbolId, price: bid, test: !options.doTrade });
+		const [ price, event ] = side === BUY ? [ openPrice, 'crypto:buy_limit' ] : [ closePrice, 'crypto:sell_limit' ];
 
-        if (!balance && balanceBTC > market.limits.cost.min) {
-            this.notify(BUY);
-        } else {
-            this.StrategyLog(`Buy event not published, balance insufisante #${symbolId} #no_balance_${symbolId}`)
-        }
-    }
-    async notifySell() {
-        const market = exchange.marketsById[this.symbolId];
-        const balance = getAssetBalance(market.base, 'free');
-        if (balance) {
-            this.notify(SELL)
-        }
-    }
-    notify(side) {
-        const { name: strategyName, options, ask: closePrice, bid: openPrice, symbolId, timeframe } = this;
-        let order = ({ strategyName, openPrice, closePrice, symbolId, timeframe });
+		if (![ 'TUSDBTC', 'BNBBTC' ].includes(symbolId)) {
+			if (price && (side === SELL || options.doTrade)) {
+				publish(event, order);
+				this.StrategyLog(`${side} event published #${symbolId}`);
+				debug(`[strategy:${strategyName}] ${side} ${symbolId} at price: ${price}`);
+			}
+		} else {
+			// this.StrategyLog("Can't trade BNB");
+		}
+	}
 
-        const [price, event] = side === BUY ? [openPrice, 'crypto:buy_limit'] : [closePrice, 'crypto:sell_limit'];
+	pairFound({ side, symbolId, price, test }) {
+		publish(`m24:algo:pair_found`, {
+			side,
+			strategyName: this.name,
+			symbolId,
+			price: `${price.toFixed(8)} `,
+			test
+		});
+		return true;
+	}
+	async getTicker({ symbolId }) {
+		let tick = await tradingView({ filter: symbolId });
+		return tick[symbolId];
+	}
 
+	async getTickers(filter = 'btc$') {
+		let tick = await tradingView({ filter });
+		return tick;
+	}
+	StrategyLog(text, options = {}) {
+		publish(`m24:algo:tracking`, { strategyName: this.name, text, ...options });
+	}
 
-        if (!['TUSDBTC', 'BNBBTC'].includes(symbolId)) {
-            if (price && (side === SELL || options.doTrade)) {
-                publish(event, order);
-                this.StrategyLog(`${side} event published #${symbolId}`)
-                debug(`[strategy:${strategyName}] ${side} ${symbolId} at price: ${price}`)
-            }
-        } else {
-            // this.StrategyLog("Can't trade BNB");
-        }
-    }
+	async findSignal({ symbolId, timeframe, position }) {
+		return findSignal({ symbolId, timeframe, position });
+	}
 
-    pairFound({ side, symbolId, price, test }) {
-        publish(`m24:algo:pair_found`, { side, strategyName: this.name, symbolId, price: `${price.toFixed(8)} `, test });
-        return true;
-    }
-    async getTicker({ symbolId }) {
-        let tick = await tradingView({ filter: symbolId });
-        return tick[symbolId]
-    }
-
-    async getTickers(filter = 'btc$') {
-        let tick = await tradingView({ filter });
-        return tick
-    }
-    StrategyLog(text, options = {}) {
-        publish(`m24:algo:tracking`, { strategyName: this.name, text, ...options });
-    }
-
-    async findSignal({ symbolId, timeframe, position }) {
-        return findSignal({ symbolId, timeframe, position })
-    }
-
-    change({ open, close }) {
-        return computeChange(open, close);
-    }
-    sorted(array) {
-        return _.reduce(array, (sorted, val, i) => {
-            let j = i + 1;
-            if (j < array.length) {
-                return sorted && array[i] < array[j];
-            }
-            return sorted;
-        }, true)
-    }
+	change({ open, close }) {
+		return computeChange(open, close);
+	}
+	sorted(array) {
+		return _.reduce(
+			array,
+			(sorted, val, i) => {
+				let j = i + 1;
+				if (j < array.length) {
+					return sorted && array[i] < array[j];
+				}
+				return sorted;
+			},
+			true
+		);
+	}
 };
