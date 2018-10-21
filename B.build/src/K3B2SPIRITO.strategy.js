3e6a2d0c4ca436ed572f7d2c9765797c840d1c24
Modeste Nana <modestemax@gmail.com>
2018-10-21 17:17:52 +0100

    spi

--- B.build/src/K3B2SPIRITO.strategy.js
+++ B.build/src/K3B2SPIRITO.strategy.js
@@ -8,16 +8,15 @@ const Template = require('./strategyBase');
 module.exports = class extends Template {
 
     async canBuy({ symbolId, timeframe }, last, prev, signal) {
-        const [currentX, lastX, prevX] = (await loadPoints({ symbolId, timeframe: this.options.timeframeX }));
-        const [candle, candle_1, candle_2, candle_3, candle_4] = (await loadPoints({ symbolId, timeframe: this.options.timeframe }));
+        const [, last, prev] = (await loadPoints({ symbolId, timeframe: this.options.timeframe }));
         let current = signal.candle;
         if (last && prev && current) {
 
-            if ((last.ema50 > last.bbb20) && (prev.ema50 <= prev.bbb20))
-                if ((current.ema100 > current.ema50) && (current.ema200 > current.ema50))
-                    if ((current.macd_distance >= last.macd_distance) && (last.macd_distance >= prev.macd_distance))
-                        if (current.bbu20 / current.bbl20 >= 1.013)
-                            if (current.close < current.ema10) {
+            if ((current.ema10 > current.ema20) && (current.ema20 >= current.ema30) && (current.ema30 >= prev.ema50))
+                if ((current.ema100 > current.ema200) && (current.ema20 > current.bbb20))
+                    if ((current.close / current.open >= 1.02) && (current.bbu20 / current.bbl20 > last.bbu20 / last.bbl20))
+                        if ((current.macd_distance > last.macd_distance) && (last.macd_distance >= prev.macd_distance))
+                            if ((current.macd_signal >= 0)) {
                                 return true;
                             }
         }
@@ -25,10 +24,10 @@ module.exports = class extends Template {
 
     async canSell({ symbolId, timeframe }, last, prev, signal) {
         let current = signal.candle;
-        const [currentX, lastX, prevX] = (await loadPoints({ symbolId, timeframe: this.options.timeframeX }));
+        const [, last, prev] = (await loadPoints({ symbolId, timeframe: this.options.timeframe }));
 
         if (current && last)
-            if (((current.ema50 < current.bb20) && (last.ema50 >= last.bbb20))) {
+            if ((current.macd_signal < last.macd_distance)) {
                 let ticker = await this.getTicker({ symbolId });
                 if (ticker && ticker.bid) {
                     debug(`${symbolId} ASK AT ${ticker.bid}`);
@@ -40,30 +39,30 @@ module.exports = class extends Template {
     }
 
     getSellPriceIfSellable(rawAsset) {
-        const { change, maxChange, openPrice, closePrice, symbolId, timestamp } = rawAsset;
-        const H1 = 1e3 * 60 * 60;
-        const M1 = 1e3 * 60;
-        const price = this.prices[symbolId];
-        const duration = Date.now() - timestamp;
+        // const { change, maxChange, openPrice, closePrice, symbolId, timestamp } = rawAsset;
+        // const H1 = 1e3 * 60 * 60;
+        // const M1 = 1e3 * 60;
+        // const price = this.prices[symbolId];
+        // const duration = Date.now() - timestamp;
 
-        if (maxChange > 0) {
-            if (change == maxChange) {
-                return false;
-            }
-        }
-        if (price) {
+        // if (maxChange > 0) {
+        //     if (change == maxChange) {
+        //         return false;
+        //     }
+        // }
+        // if (price) {
 
-            if (valuePercent(openPrice, price.bid) >= .45) {
-                return true
-            }
-        }
-        if (change < this.options.stopLoss) {
-            return true;
-        }
-        if (duration > 3 * H1) {
-            return true
-        }
+        //     if (valuePercent(openPrice, price.bid) >= .45) {
+        //         return true
+        //     }
+        // }
+        // if (change < this.options.stopLoss) {
+        //     return true;
+        // }
+        // if (duration > 3 * H1) {
+        //     return true
+        // }
 
-        return valuePercent(openPrice, this.options.takeProfit);
+        // return valuePercent(openPrice, this.options.takeProfit);
     }
 }
