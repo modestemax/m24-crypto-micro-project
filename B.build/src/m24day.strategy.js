51e57e3699c513b060d5600f717f913b00faf6a0
Modeste Nana <modestemax@gmail.com>
2018-10-24 01:14:53 +0100

    ...

--- B.build/src/m24day.strategy.js
+++ B.build/src/m24day.strategy.js
@@ -42,12 +42,12 @@ module.exports = class extends M24Base {
 	async canBuy({ symbolId, timeframe }, last, prev, signal) {
 		let current = signal.candle;
 		if (current)
-			// if (last)
+			if (last)
 			if (+current.rating >= 0)
 				if (current.position_good_spread <= this.options.min_position)
 					if (current.change_from_open > this.options.change_from_open_min)
 						if (current.change_to_high - current.change_from_open <= current.spread_percentage)
-							// if (current.close > (last.close + last.high) / 2)
+							if (current.close > (last.close + last.high) / 2)
 							if ((new Date(current.now) - new Date(current.time)) / (1e3 * 60) <
 								this.options.timeframe * 3 / 4)
 								return true;
--- F.ui/src/schemas/symbol.js
+++ F.ui/src/schemas/symbol.js
@@ -2,27 +2,27 @@ const {gql}=require('apollo-server')
 
 const typeDefs=gql`
 type Symbol  {
-    id:ID!
-    baseId:String!
-    quoteId:String!
-    symbolId:String!
-    open:Float!
-    close:Float!
-    low:Float!
-    high:Float!
-    change_from_open:Float!
-    change_to_high:Float!
-    timeframe:Int!
-    position:Int!
+    # id:Int
+    # baseId:String
+    # quoteId:String
+    symbolId:String
+    # open:Float!
+    # close:Float!
+    # low:Float!
+    # high:Float!
+    # change_from_open:Float!
+    # change_to_high:Float!
+    # timeframe:Int!
+    # position:Int!
     position_good_spread:Int
-    spread_percentage:Float!
+    # spread_percentage:Float!
 }
 type Query {
     signals:[Symbol!]!
 }
 type Market{
-    timeframe:Int!
-    market:[Symbol!]!
+    timeframe:Int
+    market:[Symbol]
 }
 type Subscription{
     signalLoaded:Market!
