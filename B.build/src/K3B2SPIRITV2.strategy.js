0eb7e554f5ecbc25b7819c040dd0dfde961b2777
Modeste Nana <modestemax@gmail.com>
2018-11-05 17:34:25 +0100

    ...

--- B.build/src/K3B2SPIRITV2.strategy.js
+++ B.build/src/K3B2SPIRITV2.strategy.js
@@ -8,18 +8,18 @@ const Template = require('./strategyBase');
 module.exports = class extends Template {
 
     async canBuy({ symbolId, timeframe }, last, prev, signal) {
-       // const [currentX, lastX, prevX] = (await loadPoints({ symbolId, timeframe: this.options.timeframeX }));
-       // const [candle, candle_1, candle_2, candle_3, candle_4] = (await loadPoints({ symbolId, timeframe: this.options.timeframe }));
+        // const [currentX, lastX, prevX] = (await loadPoints({ symbolId, timeframe: this.options.timeframeX }));
+        // const [candle, candle_1, candle_2, candle_3, candle_4] = (await loadPoints({ symbolId, timeframe: this.options.timeframe }));
         let current = signal.candle;
         if (last && prev && current) {
 
-            if((prev.ema50 >= prev.ema10) && (last.ema50 >= last.ema10) && (current.ema50 < current.ema10))
-            if((current.ema20 > current.ema30) && (current.ema10 > current.ema20) && (current.ema20 > current.bbb20))
-              if((current.macd > 0) && (current.macd >= current.macd_signal))
-                if((current.bbu20 > last.bbu20) && (current.bbl20 <= last.bbl20))
-                  if((current.close < current.bbu20)){
-                                            return true;
-                                        }
+            if ((prev.ema50 >= prev.ema10) && (last.ema50 >= last.ema10) && (current.ema50 < current.ema10))
+                if ((current.ema20 > current.ema30) && (current.ema10 > current.ema20) && (current.ema20 > current.bbb20))
+                    if ((current.macd > 0) && (current.macd >= current.macd_signal))
+                        if ((current.bbu20 > last.bbu20) && (current.bbl20 <= last.bbl20))
+                            if ((current.close < current.bbu20)) {
+                                return true;
+                            }
         }
     }
 
