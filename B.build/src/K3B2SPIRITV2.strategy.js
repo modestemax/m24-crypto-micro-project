710ecb1b1f567f3ca24f8c13a2b578a6b809224a
Modeste Nana <modestemax@gmail.com>
2018-11-06 08:29:47 +0100

    ::

--- B.build/src/K3B2SPIRITV2.strategy.js
+++ B.build/src/K3B2SPIRITV2.strategy.js
@@ -24,8 +24,8 @@ module.exports = class extends Template {
     }
 
     async canSell({ symbolId, timeframe }, last, prev, signal) {
-        let current = signal.candle;
-        const [currentX, lastX, prevX] = (await loadPoints({ symbolId, timeframe: this.options.timeframeX }));
+        // let current = signal.candle;
+        // const [currentX, lastX, prevX] = (await loadPoints({ symbolId, timeframe: this.options.timeframeX }));
 
         // if (current && last)
         //     if ((current.ema20 < current.bbb20) && (last.ema20 >= last.bbb20))
