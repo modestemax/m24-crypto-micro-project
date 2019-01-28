8d78a3e2b8fb5258db1bc35d1e880eb54cf0ab85
modestemax <modestemax@gmail.com>
2019-01-28 18:03:36 +0100

    algo bin

--- F.api/src/progress/viewProgess.js
+++ F.api/src/progress/viewProgess.js
@@ -7,7 +7,7 @@ const changePercent = (open, close) => change(open, close) * 100;
 
 module.exports = function ({ symbol, startAt }) {
     if (!viewing[symbol])
-        viewing[symbol] = { symbol, startAt }
+        viewing[symbol] = { symbol, startAt, change_max: -Infinity }
 };
 
 setInterval(function () {
@@ -15,11 +15,14 @@ setInterval(function () {
         let vs = viewing[symbol];
         vs.change_prev = vs.change;
         vs.change = changePercent(vs.startAt, prices[vs.symbol]);
+        vs.change_max = Math.max(vs.change, vs.change_max);
         if (vs.change !== vs.change_prev) {
-            console.log(`${vs.symbol} ${vs.change.toFixed(2)}%`)
+            console.log(`${vs.symbol} ${vs.change.toFixed(2)}% | ${vs.change_max}%`)
         }
-        if (vs.change < -2) {
-            delete viewing[vs.symbol]
+        if (vs.change < -2 || vs.change_max - vs.change > 2) {
+            delete viewing[vs.symbol];
+            console.log(`${vs.symbol} max= ${vs.change_max}%`)
+
         }
     }
 }, 1000);
