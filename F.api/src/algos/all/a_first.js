8f69849950944113ff8b0b183f8206441d482df2
modestemax <modestemax@gmail.com>
2019-02-26 12:04:42 +0100

    m24First

--- F.api/src/algos/all/a_first.js
+++ F.api/src/algos/all/a_first.js
@@ -1,13 +1,15 @@
 let { priceChanged: priceChangedA } = require('./a_first_a')
 let { priceChanged: priceChangedC } = require('./a_first_c')
 let { priceChanged: priceChangedD } = require('./a_first_d')
+let { priceChanged: priceChangedE } = require('./a_first_e')
 
 module.exports = {
     priceChanged() {
         [
             // priceChangedA,
             // priceChangedC,
-            priceChangedD,
+            // priceChangedD,
+            priceChangedE,
         ]
             .forEach(priceChanged =>
                 priceChanged.apply(null, arguments))
