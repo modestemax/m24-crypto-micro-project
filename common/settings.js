b4d3ac4bae3c02bc3c6ecde436e7d5d151d6911c
modestemax <modestemax@gmail.com>
2019-03-01 14:10:12 +0100

    m24First

--- common/settings.js
+++ common/settings.js
@@ -313,7 +313,7 @@ module.exports = {
             cancelBidAfterSecond: 60 * 10,//5 min,
         },
         "K3B2SPIRITOM5": {
-            isActive: true,
+            // isActive: true,
             // doTrade: true,
             timeInForce: 'GTC',
             selfStop: true,
@@ -335,7 +335,7 @@ module.exports = {
             cancelBidAfterSecond: 60 * 10,//5 min,
         },
         "K3B2SPIRITOM15": {
-            // isActive: true,
+            isActive: true,
             // doTrade: true,
             timeInForce: 'GTC',
             selfStop: true,
