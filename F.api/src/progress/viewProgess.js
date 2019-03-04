76df4c4ff6cd25c6ca6e25f3e545ea5aae64d059
modestemax <modestemax@gmail.com>
2019-03-04 11:15:31 +0100

    m24

--- F.api/src/progress/viewProgess.js
+++ F.api/src/progress/viewProgess.js
@@ -1,10 +1,10 @@
-const prices = require('../progress/prices');
 const { publish, subscribe } = require('common/redis');
 const _ = require('lodash');
 const moment = require('moment-timezone');
 const TIME_ZONE = 'Africa/Douala'
 const trades = {};
 const ONE_MIN = 1e3 * 60
+const TARGET = 2
 const change = (open, close) => (close - open) / open;
 const changePercent = (open, close) => change(open, close) * 100;
 const tme_message_ids = {}
@@ -32,20 +32,29 @@ subscribe('price', ({ symbol, close }) => {
         trade.low = _.min([trade.low, close])
         trade.oldChange = isNaN(trade.change) ? -Infinity : trade.change
         trade.change = changePercent(trade.open, trade.close)
+
         if (trade.change < -5) delete trade[symbol][trade.id]
         else {
             if (trade.change.toFixed(1) !== trade.oldChange.toFixed(1)) {
                 let highChange = changePercent(trade.open, trade.high)
                 let lowChange = changePercent(trade.open, trade.low)
+
+                const win = highChange >= TARGET
+                trade.timeEnd = trade.timeEnd || (win && Date.now()) || void 0
+                trade.minEnd = trade.minEnd || (win && trade.low) || void 0
+
+                let minEndChange = changePercent(trade.open, trade.minEnd)
+                let date = moment().tz(TIME_ZONE)
+                let quarter = Math.trunc(date.hour() / 6) + 1
                 let text = `
-#${moment().tz(TIME_ZONE).format('DDMMM')}
+#${date.format('DDMMM')} #${date.format('DDMMM')}_${quarter}
 #${trade.strategy} #${trade.strategy}_${trade.symbol}
 change ${trade.change.toFixed(2)}%
 max ${highChange.toFixed(2)}%
 min ${lowChange.toFixed(2)}%
-duration  ${moment(trade.time).fromNow()} [${moment().tz(TIME_ZONE).format('H\\h:mm')}]
-state #${highChange >= 1 ? 'win' : 'lost'}
-open ${trade.open}
+duration  ${moment(trade.time).fromNow()} [${moment(trade.time).tz(TIME_ZONE).format('H\\h:mm')}]
+state #${win ? `win [${moment(trade.timeEnd).fromNow()}] [${minEndChange.toFixed(2)}%]` : 'lost'} 
+open  ${trade.open}
 close ${trade.close}
 `
                 tme_message_ids[trade.id] && publish(`m24:algo:simulate`, {
